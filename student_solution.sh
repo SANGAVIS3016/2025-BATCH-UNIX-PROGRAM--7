652cccca0181f4a8e85087160713112640bf0431
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-08-31 10:18:56 +0530

    Create student_solution.sh

---
+++ student_solution.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Linux Practical Assignment
+# Topic: Delete Files and Directories using rm and rmdir
+
+# Complete the following commands.
+
+# 1. Delete a single file
+# Write your command below
+
+
+# 2. Delete multiple files
+# Write your command below
+
+
+# 3. Remove an empty directory
+# Write your command below
+
+
+# 4. Remove a directory containing files and subdirectories
+# Write your command below
