e6acc6463caecae65a64557f493d9b3668b773c9
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-08-31 10:24:51 +0530

    Update student_solution.sh

--- student_solution.sh
+++ student_solution.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# Linux Practical Assignment
-# Topic: Delete Files and Directories using rm and rmdir
-
-# Complete the following commands.
-
 # 1. Delete a single file
-# Write your command below
-
+rm filename.txt
 
 # 2. Delete multiple files
-# Write your command below
-
+rm file1.txt file2.txt file3.txt
 
 # 3. Remove an empty directory
-# Write your command below
-
+rmdir directory_name
 
 # 4. Remove a directory containing files and subdirectories
-# Write your command below
+rm -r nonempty_directory
